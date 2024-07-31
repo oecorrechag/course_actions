7667e7080a1a4942e44b4807724ff0bf8ad80631
oecorrechag <oecorrechag@unal.edu.co>
2024-07-31 18:12:57 -0500

    change actions

--- hello_world.py
+++
@@ -1,6 +0,0 @@
-#hello_world.py
-def hello_world():
-	print("Hello World")
-
-if __name__ == '__main__':
-	hello_world()
---
+++ hola_mundo.py
@@ -0,0 +1,9 @@
+import os
+
+def main():
+	nombre = os.getenv("USERNAME")
+	print(f"¡Hola, {nombre} desde Github!")
+
+if __name__ == '__main__':
+	main()
+	
