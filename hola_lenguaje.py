c8f3a8d714a0f733b1de2cf95d11c76abf402652
oecorrechag <oecorrechag@unal.edu.co>
2024-09-20 13:11:53 -0500

    fix 3

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    lenguaje_favorito = os.getenv("lenguaje_favorito")
+    lenguaje_favorito = os.getenv("LANGUAGE")
     print(f"¡Hola, {lenguaje_favorito} desde GitHub!")
 
 if __name__ == "__main__":
