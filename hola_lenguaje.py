cbd4711909e8b85f6f82c0864b91107dc320a294
oecorrechag <oecorrechag@unal.edu.co>
2024-09-20 16:32:27 -0500

    chang p

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     lenguaje_favorito = os.getenv("LANGUAGE")
-    print(f"¡Hola, {lenguaje_favorito} desde GitHub!")
+    print(f"¡Hola su lenguaje es:, {lenguaje_favorito} desde GitHub!")
 
 if __name__ == "__main__":
     main()
